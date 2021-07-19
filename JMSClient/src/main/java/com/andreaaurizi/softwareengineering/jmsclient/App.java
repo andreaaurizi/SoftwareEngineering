1d58ca9f41d52e6d68541da690982d28d5b86058
andreaaurizi <38937873+andreaaurizi@users.noreply.github.com>
2021-07-19 10:52:50 +0200

    Delete JMSClient directory

--- JMSClient/src/main/java/com/andreaaurizi/softwareengineering/jmsclient/App.java
+++
@@ -1,8 +0,0 @@
-package com.andreaaurizi.softwareengineering.jmsclient;
-import com.andreaaurizi.softwareengineering.jmsclient.ActionFrame;
-
-public class App {
-	public static void main(String[] args) {
-		new ActionFrame();
-	}
-}
